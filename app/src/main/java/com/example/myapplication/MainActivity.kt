24b5b816e3ba5365f3247ed771c6b91d80ba649b
duongnguyen27903 <100861773+duongnguyen27903@users.noreply.github.com>
2025-03-11 04:15:47 +0700

    feat : Section3 bai 1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,28 +1,61 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+
 import android.os.Bundle
+import android.view.Window
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
+
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContent {
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Column (
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color(0xFFDDEECF)),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+
+                ) {
+                    BusinessCard(
+                        "Nguyen Tung Duong",
+                        "Android Developer Extraordinaire"
                     )
                 }
             }
@@ -31,17 +64,77 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BusinessCard (fullName: String,title: String) {
+    Column (
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Identification(fullName,title)
+
+        Column(
+            verticalArrangement = Arrangement.Bottom,
+            modifier = Modifier.padding(bottom = 32.dp)
+        ) {
+            Contact(icon = Icons.Default.Call, text = "+1234567890")
+            Contact(icon = Icons.Default.Share, text = "@duongdev")
+            Contact(icon = Icons.Default.Email, text = "duong@gmail.com")
+        }
+    }
 }
+
+@Composable
+fun Identification(fullName : String, title : String){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize(0.8f)
+    ) {
+        Image(
+            painterResource(id = R.drawable.logo),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color(0xff073042))
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = fullName,
+            fontSize = 28.sp,
+            fontWeight = FontWeight.W300
+        )
+        Text(
+            text = title,
+            fontSize = 16.sp,
+            color = Color(0xFF388E3C),
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+fun Contact(icon: ImageVector, text: String) {
+    Row(
+        modifier = Modifier.padding(8.dp),
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription  = null,
+            tint = Color.Green
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text = text, fontSize = 16.sp, textAlign = TextAlign.Start)
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        BusinessCard(
+            "Nguyen Tung Duong",
+            "Android Developer Extraordinaire"
+        )
     }
 }
